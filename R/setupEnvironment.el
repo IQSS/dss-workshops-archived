02babb70cb0b0e78b423f9f91d08198005122fcf
Ista Zahn <istazahn@gmail.com>
2015-04-22 21:00:58 -0400

    add logo to footer

--- R/setupEnvironment.el
+++ R/setupEnvironment.el
@@ -4,10 +4,16 @@
 ;; don't let orgmode resize images (this means you must set them to the correct size when generating!)
 (setq-local org-latex-image-default-option "")
 
-;; add license note at the bottom of the page
+
 (setq-local org-confirm-babel-evaluate nil)
+
+;; add license note at the bottom of the page
 (setq-local org-html-postamble
-            "<p><a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/80x15.png' /></a><br /><span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>These IQSS statistical software workshop materials</span> by <a xmlns:cc='http://creativecommons.org/ns#' href='http://rtc.iq.harvard.edu' property='cc:attributionName' rel='cc:attributionURL'>Harvard University</a> are licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p>")
+            "<p><a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/80x15.png' /></a><br /><span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>These IQSS statistical software workshop materials</span> by <a xmlns:cc='http://creativecommons.org/ns#' href='http://rtc.iq.harvard.edu' property='cc:attributionName' rel='cc:attributionURL'>Harvard University</a> are licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p> <p> Presented by the <a href='http://rtc.iq.harvard.edu'>Research Technology Consulting Team</a> at <a href='http://iq.harvard.edu'><img alt='The Institute for Quantitative Social Science' title='IQSS logo' src='http://static.projects.iq.harvard.edu/files/styles/os_files_small/public/rtc/files/iqsslogo-2014_0.png'></img></a></p>")
+
+;; no section numbers
+(setq-local org-export-with-section-number
+            nil)
 
 ;; resize images
 (setq org-image-actual-width '(600))
