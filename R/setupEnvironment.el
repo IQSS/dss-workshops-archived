1c7cc8cd23b94a0f6b01c891c60fd3764144d06c
Ista Zahn <istazahn@gmail.com>
2015-04-22 21:11:20 -0400

    better line spacing in postamble

--- R/setupEnvironment.el
+++ R/setupEnvironment.el
@@ -9,11 +9,10 @@
 
 ;; add license note at the bottom of the page
 (setq-local org-html-postamble
-            "<p><a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/80x15.png' /></a><br /><span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>These IQSS statistical software workshop materials</span> by <a xmlns:cc='http://creativecommons.org/ns#' href='http://rtc.iq.harvard.edu' property='cc:attributionName' rel='cc:attributionURL'>Harvard University</a> are licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p> <p> Presented by the <a href='http://rtc.iq.harvard.edu'>Research Technology Consulting Team</a> at <a href='http://iq.harvard.edu'><img alt='The Institute for Quantitative Social Science' title='IQSS logo' src='http://static.projects.iq.harvard.edu/files/styles/os_files_small/public/rtc/files/iqsslogo-2014_0.png'></img></a></p>")
+            "<p><a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/80x15.png' /></a><br /><span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>These IQSS statistical software workshop materials</span> by <a xmlns:cc='http://creativecommons.org/ns#' href='http://rtc.iq.harvard.edu' property='cc:attributionName' rel='cc:attributionURL'>Harvard University</a> are licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p> <p> Presented by the <a href='http://rtc.iq.harvard.edu'>Research Technology Consulting Team</a> at <br/> <a href='http://iq.harvard.edu'><img alt='The Institute for Quantitative Social Science' title='IQSS logo' src='http://static.projects.iq.harvard.edu/files/styles/os_files_small/public/rtc/files/iqsslogo-2014_0.png'></img></a></p>")
 
 ;; no section numbers
-(setq-local org-export-with-section-number
-            nil)
+(setq-local org-export-with-section-numbers 'nil)
 
 ;; resize images
 (setq org-image-actual-width '(600))
